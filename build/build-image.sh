4d374aeb70d6379ff27d92a4efc52db658167e98
Daniil Zinenko <danil@nulana.com>
2017-07-02 00:05:15 +0300

    sync

--- build/build-image.sh
+++ build/build-image.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
 docker build --force-rm -t battleship-server -f Dockerfile .
-rm battleship
--- build/run-mysql.sh
+++ build/run-mysql.sh
@@ -3,10 +3,10 @@
 docker stop battleship-db | true
 docker rm battleship-db | true
 docker run -d --name battleship-db \
-#    -v /private/var/battleship/mysql:/var/lib/mysql \
-#    -e MYSQL_DATABASE=battleship \
-#    -e MYSQL_ALLOW_EMPTY_PASSWORD=yes \
-#    -e MYSQL_ROOT_HOST=172.25.0.3 \
-#    --network=battleship-network \
-#    --ip=172.25.0.2 \
+    -v /private/var/battleship/mysql:/var/lib/mysql \
+    -e MYSQL_DATABASE=battleship \
+    -e MYSQL_ALLOW_EMPTY_PASSWORD=yes \
+    -e MYSQL_ROOT_HOST=172.25.0.3 \
+    --network=battleship-network \
+    --ip=172.25.0.2 \
     mysql/mysql-server
